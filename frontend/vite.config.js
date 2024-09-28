2246e7ea7a06474e40349db0a7e55618baadae81
KabTheProgrammer <ikabiru038@gmai.com>
2024-09-28 02:19:20 +0000

    Updated Vite configuration and built the project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true }), // This will open the visualizer after build
+  ],
   server: {
     proxy: {
       "/api/": "http://localhost:5000",
       "/uploads/": "http://localhost:5000",
+    },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: id => {
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0].toString(); // Create a chunk for each module in node_modules
+          }
+        },
+      },
     },
   },
 });
